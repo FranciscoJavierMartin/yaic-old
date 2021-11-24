5f49ef3dc1f8b92aa7d954e830ff773e518621df
Francisco <javiermartincuenca@gmail.com>
2021-11-24 21:21:42 +0100

    Add tailwind css

--- client/components/Post.tsx
+++ client/components/Post.tsx
@@ -7,14 +7,14 @@ interface PostProps {
 
 export default function Post({ post }: PostProps) {
   return (
-    <div className="w-1/3 p-px md:px-3">
+    <div className="w-full sm:w-1/2 lg:w-1/3 p-px md:px-3">
       <a href="#">
-        <article className="post bg-gray-100 text-white relative pb-full md:mb-6">
+        <article className="post bg-gray-100 text-white relative md:mb-6">
           <Image
             className="w-full h-full absolute left-0 top-0 object-cover"
             src={post.imageUrl}
             alt="image"
-            layout="fill"
+            layout="responsive"
             height="500"
             width="500"
           />
