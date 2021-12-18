6591c5ef07fa80a323af46562cfcc010f0440e2b
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2021-12-18 11:09:46 +0100

    Add more dummy posts

--- client/components/post-list/PostList.tsx
+++ client/components/post-list/PostList.tsx
@@ -5,7 +5,7 @@ import styles from './PostList.module.scss';
 export default function PostList() {
   return (
     <div className={styles.postList} data-testid='post-list'>
-      {[1, 2, 3].map((index) => (
+      {[1, 2, 3, 4, 5, 6, 7, 8].map((index) => (
         <Post key={index} />
       ))}
     </div>
