a767daea289d34b57e79ea5d5298a28a860a9441
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2021-12-21 20:01:04 +0100

    Setup tests for api

--- apps/api/test/api.test.ts
+++ apps/api/test/api.test.ts
@@ -1,5 +1,18 @@
+import * as supertest from 'supertest';
+import app from '../src/app';
+
+const requestWithSupertest = supertest(app);
+
 describe('Example describe', () => {
   test('Example test', () => {
     expect(1).toBe(1);
+  });
+});
+
+describe('Generic endpoints', () => {
+  test('GET /api', async () => {
+    // requestWithSupertest.get('/api');
+    const res = await requestWithSupertest.get('/api');
+    expect(res.status).toEqual(200);
   });
 });
