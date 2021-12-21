d8b3622e09558596f4d738ac4bd290d0b0216377
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2021-12-21 22:13:41 +0100

    fix unit tests for client

--- apps/client/__tests__/pages/index.test.tsx
+++ apps/client/__tests__/pages/index.test.tsx
@@ -1,16 +1,15 @@
- import React from 'react'
- import { render, screen } from '@testing-library/react'
- import Home from '../../pages/index'
- 
- describe('Home', () => {
-   it('renders a Navbar', () => {
-     render(<Home />)
- 
-     const heading = screen.getByRole('heading', {
-       name: 'Yaic',
-     })
- 
-     expect(heading).toBeInTheDocument()
-   })
- })
- 
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Home from '../../pages/index';
+
+describe('Home', () => {
+  it('renders a Navbar', () => {
+    render(<Home />);
+
+    const heading = screen.getByRole('heading', {
+      name: 'Yaic',
+    });
+
+    expect(heading).toBeDefined();
+  });
+});
--- apps/client/jest.config.js
+++ apps/client/jest.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   displayName: 'client',
   preset: '../../jest.preset.js',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
