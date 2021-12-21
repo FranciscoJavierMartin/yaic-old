eed489667972d4460e7ff8aea4f0ae570bcae71a
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2021-12-21 23:25:30 +0100

    Fixed Expected modern color-function notation

--- apps/client/components/post-detail/PostDetail.tsx
+++ apps/client/components/post-detail/PostDetail.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import Image from 'next/image';
 import CommentSemantic from '../comments/Comments';
-import styles from './PostDetail.module.scss';
 
 export default function PostDetail() {
   return (
-    <article className={styles.postDetail}>
+    <article>
       <Image
         src='https://images.unsplash.com/photo-1502791451862-7bd8c1df43a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
         layout='intrinsic'
