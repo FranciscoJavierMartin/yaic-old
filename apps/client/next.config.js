2ddd3c76ab956b240372b8ce3ec74d20819b9564
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2021-12-21 21:29:02 +0100

    Move code

--- apps/client/next.config.js
+++ apps/client/next.config.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withNx = require('@nrwl/next/plugins/with-nx');
+const path = require('path');
 
 /**
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
@@ -9,6 +10,14 @@ const nextConfig = {
     // Set this to true if you would like to to use SVGR
     // See: https://github.com/gregberge/svgr
     svgr: false,
+  },
+  reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+    prependData: `@import "variables.scss";`,
+  },
+  images: {
+    domains: ['images.unsplash.com'],
   },
 };
 
--- client/pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import type { AppProps } from 'next/app';
-import '../styles/globals.scss';
-
-function CustomApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
-
-export default CustomApp;
