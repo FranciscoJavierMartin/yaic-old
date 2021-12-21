78e1aa057f9b251cb72ce9e8babcd1952b74d24f
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2021-12-21 20:46:39 +0100

    Refactor _app

--- apps/client/pages/_app.tsx
+++ apps/client/pages/_app.tsx
@@ -1,18 +1,8 @@
-import { AppProps } from 'next/app';
-import Head from 'next/head';
-import './styles.css';
+import type { AppProps } from 'next/app';
+import '../styles/globals.scss';
 
 function CustomApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <title>Welcome to client!</title>
-      </Head>
-      <main className="app">
-        <Component {...pageProps} />
-      </main>
-    </>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default CustomApp;
